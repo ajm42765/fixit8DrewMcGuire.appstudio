2c8a385418a4d3cfa1905b7ea57a0a7b1f0c6dc2
ajm42765 <60629760+ajm42765@users.noreply.github.com>
2020-02-19 16:14:19 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# fixit8DrewMcGuire.appstudio
+ 
---
+++ forms/whileAB/whileAB.js
@@ -0,0 +1,7 @@
+//get first and last name
+let userName = prompt(`enter your name`)
+let i = 0
+
+while(i == userName.length)
+  console.log(`Letter i+1 is userName.charAt`)
+  
